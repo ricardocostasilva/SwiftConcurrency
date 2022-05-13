8cd68fc3e19c7afcfbd7bdedfc9fd04d883d7e02
Ricardo Silva <ricardo@ricardocsiva.com>
2022-05-14 00:23:35 +0100

    Initial commit

---
+++ SwiftConcurrency/SwiftConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurrencyApp.swift
+//  SwiftConcurrency
+//
+//  Created by ricardo silva on 14/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
